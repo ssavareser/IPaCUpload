6ea8449cc3088866b19f0b01c0f057e133bfee86
ssavareser <sydney.savarese@radia.com>
2024-08-01 12:09:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
-import os
 import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-
-# Function to install ChromeDriver
-@st.experimental_singleton
-def install_chromedriver():
-    os.system('sbase install chromedriver')
-    os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/chromedriver /home/appuser/venv/bin/chromedriver')
-
-# Install ChromeDriver once
-_ = install_chromedriver()
 
 # Configure Chrome options
 chrome_options = Options()
